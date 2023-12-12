b3f53ed7ef30589eff7b824de64dc0d39efa3718
Razvan Baboiu <razvanbaboiu@gmail.com>
2023-12-13 00:10:57 +0200

    prevent auto zooming from browser

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <template>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  </head>
   <NuxtLoadingIndicator /> 
   <NuxtLayout>
     <NuxtPage/>
